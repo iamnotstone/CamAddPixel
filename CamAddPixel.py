0dfbc89f143df799251a0910c01a15454dd2641a
iamnotstone <iamnotstone@sina.com>
2015-12-15 16:34:24 +0900

    Update CamAddPixel.py

--- CamAddPixel.py
+++ CamAddPixel.py
@@ -7,14 +7,28 @@ SIZE = (640, 480)
 
 def generateSque():
     a = []
-    for i in range(10000):
+    for i in range(640):
         a.append((255,0,0))
     return a
 
-
+def getRectFromImage(LeftTop,RightButtom):
+    dataAry = []
+    for i in range(LeftTop[1],RightButtom[1]):
+        raw = i
+        if raw == LeftTop[1]:
+            for j in range(LeftTop[0],RightButtom[0]):
+                dataAry.append((j,i))
+        elif raw == RightButtom[1] - 1:
+            for j in range(LeftTop[0],RightButtom[0]):
+                dataAry.append((j,i))
+        else:
+            dataAry.append((LeftTop[0],i))
+            dataAry.append((RightButtom[0] - 1,i))
+    return dataAry
+    
 def get_image():
     # Put your code here to return a PIL image from the camera.
-    cam = Device(0)
+    cam = Device(1)
     
     #return Image.new('L', SIZE)
     return cam.getImage(1)
@@ -35,13 +49,19 @@ class Panel(wx.Panel):
     def update(self):
         self.Refresh()
         self.Update()
-        wx.CallLater(100, self.update)
+        wx.CallLater(10, self.update)
     def create_bitmap(self):
         image = get_image()
         #image = image.convert('L')
         #print list(image.getdata())
         a = generateSque()
-        image.putdata(a)
+        dataAry = getRectFromImage((100,200),(400,400))
+        #print len(dataAry)
+        #print len(list(image.getdata()))
+        for pos in dataAry:
+            image.putpixel(pos,(255,0,0))
+        
+            
         bitmap = pil_to_wx(image)
         return bitmap
     def on_paint(self, event):
